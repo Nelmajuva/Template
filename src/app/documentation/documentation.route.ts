ac5e6eda5bb01c335c10ae7dc779be0ff2df034f
Juan Camilo López Morales <camilopezm24734m@gmail.com>
2026-04-18 22:13:33 -0500

    feat: avances

--- src/app/documentation/documentation.route.ts
+++ src/app/documentation/documentation.route.ts
@@ -2,16 +2,24 @@ import { Routes } from '@angular/router';
 
 export const documentationRoutes: Routes = [
   {
-    path: 'buttons',
-    loadComponent: () => import('./pages/buttons/buttons').then((x) => x.Buttons),
-  },
-  {
     path: 'accordion',
     loadComponent: () => import('./pages/accordion/accordion').then((x) => x.Accordion),
   },
   {
     path: 'alerts',
     loadComponent: () => import('./pages/alerts/alerts').then((x) => x.Alerts),
+  },
+  {
+    path: 'buttons',
+    loadComponent: () => import('./pages/buttons/buttons').then((x) => x.Buttons),
+  },
+  {
+    path: 'forms',
+    loadComponent: () => import('./pages/forms/forms').then((x) => x.Forms),
+  },
+  {
+    path: 'modals',
+    loadComponent: () => import('./pages/modals/modals').then((x) => x.Modals),
   },
   {
     path: '**',
---
+++ src/app/documentation/pages/forms/forms.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { LucideChessQueen } from '@lucide/angular';
+
+@Component({
+  selector: 'app-forms',
+  imports: [LucideChessQueen],
+  templateUrl: './forms.html',
+  styleUrl: './forms.scss',
+})
+export class Forms {}
